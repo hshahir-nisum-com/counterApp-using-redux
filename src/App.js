2c9305e917027d550e6befe560ec7ec146cfdf44
Hafiz Shahir Zain <hshahir@nisum.com>
2020-12-23 20:11:38 +0500

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import './App.css';
+import store from './functions/store/index';
+import { useDispatch } from 'react-redux';
+import {add} from './functions/actions';
 
 function App() {
+  console.log(store.getState())
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
         <div>
             <span> Number </span> <br/>
             <div className="wrapper">
-            <input type="button" value="Increment" className="btn"/>
+            <input type="button" value="Increment" className="btn"
+            onClick={() => {
+              dispatch(add())
+            }}
+             />
             <input type="button" value="Decrement" className="btn"/>
             </div>
         </div>
--- src/functions/reducers/index.js
+++ src/functions/reducers/index.js
@@ -1,9 +1,10 @@
 import { type } from "../actions/type";
 
-export  const reducer = (state={}, action)=>{
+ export   const reducer = (state={}, action)=>{
     switch (action.type) {
-        case type.add :{
+        case type.add: {
             return (state+1)
+            debugger;
         }
         case type.dec :{
             return (state-1)
